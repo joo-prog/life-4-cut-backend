99b6d45182445c4dba0ddcf171c84f36b92a84c6
Kim Myeongil <mikim961103@gmail.com>
2023-08-12 15:53:56 +0900

    feat: 샘플 querydsl 조회 쿼리 추가

--- src/main/java/com/onebyte/life4cut/sample/repository/SampleRepository.java
+++ src/main/java/com/onebyte/life4cut/sample/repository/SampleRepository.java
@@ -1,8 +1,16 @@
 package com.onebyte.life4cut.sample.repository;
 
+import com.onebyte.life4cut.common.vo.Email;
 import com.onebyte.life4cut.sample.domain.Sample;
+import com.onebyte.life4cut.sample.repository.dto.SampleSearchDto;
+
+import java.util.List;
 
 public interface SampleRepository {
 
     Sample save(Sample sample);
+
+    List<Sample> findByEmail(Email email);
+
+    List<SampleSearchDto> search(Email email);
 }
--- src/test/java/com/onebyte/life4cut/sample/repository/SampleRepositoryImplTest.java
+++ src/test/java/com/onebyte/life4cut/sample/repository/SampleRepositoryImplTest.java
@@ -3,6 +3,7 @@ package com.onebyte.life4cut.sample.repository;
 import com.onebyte.life4cut.common.vo.Email;
 import com.onebyte.life4cut.fixture.SampleFixtureFactoryFactory;
 import com.onebyte.life4cut.sample.domain.Sample;
+import com.onebyte.life4cut.sample.repository.dto.SampleSearchDto;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
@@ -58,4 +62,25 @@ class SampleRepositoryImplTest {
         }
     }
 
+    @Test
+    @DisplayName("이메일로 샘플을 조회한다")
+    void search() {
+        // given
+        Sample sample = sampleFixtureFactory.save((entity, builder) -> {
+            builder.setNull("id");
+            builder.set("email", Email.of("test@gmail.com"));
+            builder.set("nickname", "nickname");
+            builder.setNull("createdAt");
+            builder.setNull("updatedAt");
+        });
+
+        // when
+        List<SampleSearchDto> search = sampleRepository.search(Email.of("test@gmail.com"));
+
+        // then
+        assertThatList(search).hasSize(1);
+        assertThat(search.get(0).id()).isEqualTo(sample.getId());
+        assertThat(search.get(0).email()).isEqualTo(sample.getEmail());
+        assertThat(search.get(0).nickname()).isEqualTo(sample.getNickname());
+    }
 }
