92648c241256e24edddcfadad8b0c03cecfb9972
Kim Myeongil <mikim961103@gmail.com>
2023-08-19 17:13:29 +0900

    feat: spring security kakao login 구현

---
+++ src/main/java/com/onebyte/life4cut/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.onebyte.life4cut.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login((oauth2) -> oauth2
+                        // 로그인 성공시 이동할 페이지
+                        .defaultSuccessUrl("https://www.naver.com", true)
+                )
+                .build();
+    }
+}
