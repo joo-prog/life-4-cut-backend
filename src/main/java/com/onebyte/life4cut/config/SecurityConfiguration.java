ccf5442561b5aaf389feedb01c8bce07eac54ce7
username1103 <mikim961103@gmail.com>
2023-08-26 17:05:29 +0900

    feat: naver login 추가

--- src/main/java/com/onebyte/life4cut/config/SecurityConfiguration.java
+++ src/main/java/com/onebyte/life4cut/config/SecurityConfiguration.java
@@ -1,11 +1,18 @@
 package com.onebyte.life4cut.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.io.IOException;
 
 @Configuration
 @EnableRedisHttpSession
@@ -22,9 +29,20 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated()
                 )
                 .oauth2Login((oauth2) -> oauth2
-                        // 로그인 성공시 이동할 페이지
-                        .defaultSuccessUrl("/oauth-success", true)
+                        // 로그인 페이지
+//                        .loginPage("frontend-login-page")
+                                .successHandler(new CustomSuccessHandler())
                 )
                 .build();
     }
+
+    private static class CustomSuccessHandler implements AuthenticationSuccessHandler {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+            // 로그인 성공시 처리 추가
+            response.sendRedirect("/oauth-success");
+        }
+    }
+
+
 }
