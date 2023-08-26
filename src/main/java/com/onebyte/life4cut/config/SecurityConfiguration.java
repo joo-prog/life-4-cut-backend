1c653657ca103baf7f72d170e93d86fff39c4493
Kim Myeongil <mikim961103@gmail.com>
2023-08-26 15:04:42 +0900

    chore: redis session repository를 사용하도록 수정

--- src/main/java/com/onebyte/life4cut/config/SecurityConfiguration.java
+++ src/main/java/com/onebyte/life4cut/config/SecurityConfiguration.java
@@ -3,12 +3,12 @@ package com.onebyte.life4cut.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableWebSecurity
+@EnableRedisHttpSession
 public class SecurityConfiguration {
 
 
