2a77a494a7a41afe27419b5fce0aa98807ff3b21
pythonstrup <pythonstrup@gmail.com>
2023-09-08 22:42:00 +0900

    feat: User Entity

---
+++ src/main/java/com/onebyte/life4cut/user/domain/User.java
@@ -0,0 +1,52 @@
+package com.onebyte.life4cut.user.domain;
+
+import com.onebyte.life4cut.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "unique_user_oauth_index",
+            columnNames = {
+                "oauth_type",
+                "oauth_id",
+            }
+        ),
+        @UniqueConstraint(
+            name = "unique_user_nickname_index",
+            columnNames = {"nickname"}
+        )
+    }
+)
+public class User extends BaseEntity {
+
+  @Column(unique = true, length = 30)
+  private String nickname;
+
+  @Column(length = 255)
+  private String profile_path;
+
+  private String email;
+
+  @Column(length = 30)
+  private String oauthType;
+
+  @Column(length = 30)
+  private String oauthId;
+
+  private LocalDateTime deletedAt;
+
+  public void deleteSoftly(LocalDateTime deletedAt) {
+    this.deletedAt = deletedAt;
+  }
+}
