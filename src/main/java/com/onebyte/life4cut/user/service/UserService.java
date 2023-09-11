29e996f5035b9042160799e14818f619e666ac00
pythonstrup <pythonstrup@gmail.com>
2023-09-11 21:50:45 +0900

    feat: UserService findUserByOAuthInfo

--- src/main/java/com/onebyte/life4cut/user/service/UserService.java
+++ src/main/java/com/onebyte/life4cut/user/service/UserService.java
@@ -1,8 +1,12 @@
 package com.onebyte.life4cut.user.service;
 
+import com.onebyte.life4cut.auth.dto.OAuthInfo;
 import com.onebyte.life4cut.user.domain.User;
 import com.onebyte.life4cut.user.exception.UserNotFound;
+import com.onebyte.life4cut.user.exception.UserNotUnique;
 import com.onebyte.life4cut.user.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +20,13 @@ public class UserService {
     User user = userRepository.findUser(id)
         .orElseThrow(UserNotFound::new);
     return user;
+  }
+
+  public Optional<User> findUserByOAuthInfo(OAuthInfo oAuthInfo) {
+    List<User> result = userRepository.findUserByOAuthInfo(oAuthInfo);
+    if (result.size() > 1) {
+      throw new UserNotUnique();
+    }
+    return result.stream().findAny();
   }
 }
