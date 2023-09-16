cd484315b15a8494b3446d808a5a48f2d9d57e7e
pythonstrup <pythonstrup@gmail.com>
2023-09-16 14:37:10 +0900

    chore: SecurityConfig 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.session:spring-session-data-redis")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
