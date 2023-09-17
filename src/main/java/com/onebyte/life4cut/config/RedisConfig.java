7db9d5c6ef7489121bb2252cb60a4d468d96c956
pythonstrup <pythonstrup@gmail.com>
2023-09-17 20:11:34 +0900

    chore: RedisConfig 수정
    - password 설정 추가

--- src/main/java/com/onebyte/life4cut/config/RedisConfig.java
+++ src/main/java/com/onebyte/life4cut/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -12,17 +13,24 @@ public class RedisConfig {
 
   private final String redisHost;
   private final int redisPort;
+  private final String redisPassword;
 
   public RedisConfig(
       @Value("${spring.data.redis.host}") String redisHost,
-      @Value("${spring.data.redis.port}") int redisPort) {
+      @Value("${spring.data.redis.port}") int redisPort,
+      @Value("${spring.data.redis.password}") String redisPassword) {
     this.redisHost = redisHost;
     this.redisPort = redisPort;
+    this.redisPassword = redisPassword;
   }
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(redisHost, redisPort);
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+    config.setHostName(redisHost);
+    config.setPort(redisPort);
+    config.setPassword(redisPassword);
+    return new LettuceConnectionFactory(config);
   }
 
   @Bean
