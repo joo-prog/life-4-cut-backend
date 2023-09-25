be4e19019df8fdaae0bcd8306301b5c18c8d18b1
pythonstrup <pythonstrup@gmail.com>
2023-09-25 21:27:42 +0900

    chore: SwaggerUI 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.hidetake.gradle.swagger.generator.GenerateSwaggerUI
+
 plugins {
     java
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
-    id("org.asciidoctor.jvm.convert") version "3.3.2"
     id("jacoco")
+    id("com.epages.restdocs-api-spec") version "0.18.2"
+    id("org.hidetake.swagger.generator") version "2.18.2"
 }
 
 group = "com.onebyte"
@@ -26,7 +29,6 @@ repositories {
 extra["snippetsDir"] = file("build/generated-snippets")
 
 val querydslDir = "src/main/generated"
-val asciidoctorExt: Configuration by configurations.creating
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -54,13 +56,28 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:0.6.3")
-    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
+
+    // open api swagger
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.18.2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    swaggerUI("org.webjars:swagger-ui:4.11.1")
 
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:localstack")
 }
+
+
+openapi3 {
+    setServer("http://localhost:8080")
+    title = "spring-rest-docs + Swagger-UI"
+    description = "Swagger UI"
+    version = "0.0.1"
+    format = "json"
+    outputDirectory = "build/resources/main/static/docs"
+}
+
 
 sourceSets {
     getByName("main").java.srcDirs(querydslDir)
@@ -68,6 +85,15 @@ sourceSets {
 
 tasks.withType<JavaCompile> {
     options.generatedSourceOutputDirectory.set(file(querydslDir))
+}
+
+tasks.withType<GenerateSwaggerUI> {
+    dependsOn("openapi3")
+    delete(file("src/main/resources/static/docs/"))
+    copy {
+        from("$buildDir/resources/main/static/docs")
+        into("src/main/resources/static/docs/")
+    }
 }
 
 tasks {
@@ -89,29 +115,16 @@ tasks {
     }
 
     /**
-     * REST Docs & Asciidoc
+     * REST Docs
      */
-    asciidoctor {
-        configurations(asciidoctorExt.name)
-        inputs.dir(snippetsDir)
-        dependsOn(test)
-    }
-
     bootJar {
-        dependsOn(asciidoctor)
-        from("build/docs/asciidoc") {
-            into("static/docs")
-        }
+        dependsOn("openapi3")
     }
 
-    register<Copy>("copyAsciidoctor") {
-        dependsOn(asciidoctor)
-        from(file("$buildDir/docs/asciidoc"))
-        into(file("src/main/resources/static/docs"))
-    }
-
-    build {
-        dependsOn("copyAsciidoctor")
+    register<Copy>("copySwaggerUI") {
+        dependsOn("generateSwaggerUI")
+        from("$buildDir/resources/static/docs")
+        into("src/main/resources/static/docs")
     }
 
     /**
