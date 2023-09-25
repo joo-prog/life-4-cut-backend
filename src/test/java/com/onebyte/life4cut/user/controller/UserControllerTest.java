0a72db55e292bfc8ff780755c784c261d23f2a3a
pythonstrup <pythonstrup@gmail.com>
2023-09-25 21:42:49 +0900

    feat: Test SwaggerUI 적용

--- src/test/java/com/onebyte/life4cut/user/controller/UserControllerTest.java
+++ src/test/java/com/onebyte/life4cut/user/controller/UserControllerTest.java
@@ -1,43 +1,38 @@
 package com.onebyte.life4cut.user.controller;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
 import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.onebyte.life4cut.common.annotation.WithCustomMockUser;
 import com.onebyte.life4cut.common.controller.ControllerTest;
-import com.onebyte.life4cut.config.TestSecurityConfiguration;
 import com.onebyte.life4cut.user.controller.dto.UserFindResponse;
 import com.onebyte.life4cut.user.domain.User;
 import com.onebyte.life4cut.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 @WebMvcTest(UserController.class)
-@ExtendWith({MockitoExtension.class, RestDocumentationExtension.class, SpringExtension.class})
-@ImportAutoConfiguration(TestSecurityConfiguration.class)
 class UserControllerTest extends ControllerTest {
 
   @MockBean
@@ -73,18 +68,27 @@ class UserControllerTest extends ControllerTest {
         .andExpect(jsonPath("$.message").value("OK"))
         .andExpect(jsonPath("$.data", equalTo(asParsedJson(result))))
         .andDo(
-            document(
+            MockMvcRestDocumentationWrapper.document(
                 "{class_name}/{method_name}",
-                queryParameters(
-                    parameterWithName("nickname").description("유저 닉네임")
-                ),
-                responseFields(
-                    fieldWithPath("message").type(STRING).description("응답 메시지"),
-                    fieldWithPath("data").type(OBJECT).description("데이터"),
-                    fieldWithPath("data.userId").type(NUMBER).description("유저 ID"),
-                    fieldWithPath("data.nickname").type(STRING).description("유저 닉네임"),
-                    fieldWithPath("data.email").type(STRING).description("유저 이메일"),
-                    fieldWithPath("data.profilePath").type(STRING).description("유저 프로필 사진 파일 경로")
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .description("닉네임으로 유저 조회 API")
+                        .queryParameters(
+                            parameterWithName("nickname").description("유저 닉네임")
+                        )
+                        .requestFields()
+                        .responseFields(
+                            fieldWithPath("message").type(STRING).description("응답 메시지"),
+                            fieldWithPath("data").type(OBJECT).description("데이터"),
+                            fieldWithPath("data.userId").type(NUMBER).description("유저 ID"),
+                            fieldWithPath("data.nickname").type(STRING).description("유저 닉네임"),
+                            fieldWithPath("data.email").type(STRING).description("유저 이메일"),
+                            fieldWithPath("data.profilePath").type(STRING)
+                                .description("유저 프로필 사진 파일 경로")
+                        )
+                        .build()
                 )
             )
         )
@@ -116,15 +120,23 @@ class UserControllerTest extends ControllerTest {
         .andExpect(jsonPath("$.message").value("OK"))
         .andExpect(jsonPath("$.data", equalTo(asParsedJson(result))))
         .andDo(
-            document(
+            MockMvcRestDocumentationWrapper.document(
                 "{class_name}/{method_name}",
-                responseFields(
-                    fieldWithPath("message").type(STRING).description("응답 메시지"),
-                    fieldWithPath("data").type(OBJECT).description("데이터"),
-                    fieldWithPath("data.userId").type(NUMBER).description("유저 ID"),
-                    fieldWithPath("data.nickname").type(STRING).description("유저 닉네임"),
-                    fieldWithPath("data.email").type(STRING).description("유저 이메일"),
-                    fieldWithPath("data.profilePath").type(STRING).description("유저 프로필 사진 파일 경로")
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .description("마이페이지 정보 조회 API")
+                        .requestFields()
+                        .responseFields(
+                            fieldWithPath("message").type(STRING).description("응답 메시지"),
+                            fieldWithPath("data").type(OBJECT).description("데이터"),
+                            fieldWithPath("data.userId").type(NUMBER).description("유저 ID"),
+                            fieldWithPath("data.nickname").type(STRING).description("유저 닉네임"),
+                            fieldWithPath("data.email").type(STRING).description("유저 이메일"),
+                            fieldWithPath("data.profilePath").type(STRING).description("유저 프로필 사진 파일 경로")
+                        )
+                        .build()
                 )
             )
         )
